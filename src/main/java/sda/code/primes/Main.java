ce8d580130fc557c40f52eca8a8f42e449581220
Artur Czajka <a.czajka@oberthur.com>
2018-01-14 13:22:26 +0000

    Bez mocka i z klasą

--- src/main/java/sda/code/primes/Main.java
+++ src/main/java/sda/code/primes/Main.java
@@ -1,29 +1,33 @@
 package sda.code.primes;
 
 import java.util.Scanner;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class Main {
     private final Supplier<Integer> input;
-    private final PrimeChecker primeChecker;
+    private final Predicate<Integer> primeChecker;
 
     public static void main(String[] args) {
         try (Scanner scan = new Scanner(System.in)) {
             new Main(
                     () -> scan.nextInt(),
-                    new PrimeChecker()
+//                  new PrimeChecker()
+                    x -> new PrimeChecker().isPrime(x)
             ).run();
         }
     }
 
-    public Main(Supplier<Integer> input, PrimeChecker primeChecker) {
+//  public Main(Supplier<Integer> input, PrimeChecker primeChecker) {
+    public Main(Supplier<Integer> input, Predicate<Integer> primeChecker) {
         this.input = input;
         this.primeChecker = primeChecker;
     }
 
     void run() {
         Integer number = input.get();
-        boolean isPrime = primeChecker.isPrime(number);
+//      boolean isPrime = primeChecker.isPrime(number);
+        boolean isPrime = primeChecker.test(number);
         if (isPrime) {
             success();
         } else {
--- src/test/java/sda/code/primes/MainTest.java
+++ src/test/java/sda/code/primes/MainTest.java
@@ -3,21 +3,71 @@ package sda.code.primes;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Predicate;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class MainTest {
+
+//    @Test
+//    public void jestPiersze() {
+//        PrimeChecker checker = Mockito.mock(PrimeChecker.class);
+//        Mockito.when(checker.isPrime(Mockito.anyInt())).thenReturn(true);
+//
+//        Main main = Mockito.spy(new Main(
+//                () -> 0,
+//                checker
+//        ));
+//
+//        main.run();
+//
+//        Mockito.verify(checker).isPrime(0);
+//
+//        Mockito.verify(main).success();
+//        Mockito.verify(main, Mockito.never()).failure();
+//    }
+
+    static class AlwaysTruePredicate implements Predicate<Integer> {
+
+        private final AtomicBoolean predicateWasRun;
+        private final int expectedInt;
+
+        public AlwaysTruePredicate(AtomicBoolean predicateWasRun, Integer expectedInt) {
+            this.predicateWasRun = predicateWasRun;
+            this.expectedInt = expectedInt;
+        }
+
+        @Override
+        public boolean test(Integer x) {
+            predicateWasRun.set(true);
+            assertEquals(expectedInt, x.intValue());
+            return true;
+        }
+    }
+//
+//    public boolean test(Integer x) {
+//        return x == 0;
+//    }
 
     @Test
     public void jestPiersze() {
-        PrimeChecker checker = Mockito.mock(PrimeChecker.class);
-        Mockito.when(checker.isPrime(Mockito.anyInt())).thenReturn(true);
+        AtomicBoolean predicateWasRun = new AtomicBoolean(false);
 
         Main main = Mockito.spy(new Main(
                 () -> 0,
-                checker
+                new AlwaysTruePredicate(predicateWasRun, 0)
+//                this::test
         ));
 
         main.run();
 
-        Mockito.verify(checker).isPrime(0);
+        assertTrue(predicateWasRun.get());
 
         Mockito.verify(main).success();
         Mockito.verify(main, Mockito.never()).failure();
